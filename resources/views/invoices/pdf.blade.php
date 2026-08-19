b292b0c8ee86cd8cb0577f66c8155cfb5733e218
Luxiusgg365 <luciusyr1@gmail.com>
2026-08-19 12:18:06 +0700

    chore: remove orphaned invoice-related files and unused console commands after Invoice feature removal

--- resources/views/invoices/pdf.blade.php
+++
@@ -1,5 +0,0 @@
-@php $outputMode='pdf'; @endphp
-@include('rentals.partials.premium-doc-head', ['docTitle' => 'Invoice ' . ($invoice->invoice_number ?? ''), 'outputMode' => $outputMode])
-@include('rentals.partials.doc-parts-invoice', ['rental' => $invoice])
-@include('rentals.partials.doc-close-html')
-
